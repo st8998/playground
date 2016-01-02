d686c4cde718a46e288b8a89aa47f53ddfdae31f
Ivan Efremov <st8998@gmail.com>
2016-01-02 17:46:33 +0400

    rewrite generator

--- src/generator.js
+++ src/generator.js
@@ -1,10 +1,15 @@
-import { into, map, identity } from 'ramda'
+import { into, map, identity, transduce, uncurryN } from 'ramda'
 
 function* numbers(start, end) {
   for(let curr = start; curr <= end; curr++)
     yield curr
 }
 
-const arr = into([], map(identity), numbers(1, 3))
+const noop = identity
 
-console.log(arr)
+const transEach = uncurryN(2, func => transduce(identity, (_, val)=> func(val), null))
+
+const each = transEach(::console.log)
+
+each(numbers(1, 3)) //=> 1 2 3
+each([1,2,3,4]) //=> 1 2 3 4
